34ff1cf5b8507c72d2c9e4be1d86221cfc8ee8a2
xcxerxes <vic19910108@gmail.com>
2022-12-02 22:13:13 +0800

    feat: 删除old

--- ibooks-admin/src/utils/token.ts
+++ ibooks-admin/src/utils/token.ts
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import Cookie from 'js-cookie'
-const TOKEN_KEY = 'authrozation'
+const TOKEN_KEY = 'Authorization'
 export const setToken = (value: any) => {
   Cookie.set(TOKEN_KEY, value)
 }
